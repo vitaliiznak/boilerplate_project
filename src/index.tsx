99090b8fa448799ae3f41e9c3863bd0ccf4f819e
David Barrat <david.barrat.1@gmail.com>
2019-07-13 00:30:43 +0200

    fix import react-vis

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import 'antd/dist/antd.css'
 import App from './App'
 import './index.css'
-import '../node_modules/react-vis/dist/style.css'
+import 'react-vis/dist/style.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
